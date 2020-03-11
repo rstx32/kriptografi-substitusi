720a4e7cd069bf69d71a4cbe4bcbf7ddf6e26119
rstx <rstx32@gmail.com>
2020-03-11 21:30:49 +0700

    commit pertama

---
+++ app/src/main/java/com/example/substitusi_kriptografi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.substitusi_kriptografi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
