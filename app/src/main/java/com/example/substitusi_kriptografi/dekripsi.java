0eeeeb0b5d688eddd02c163f9ff4db9bb08e94d5
rstx <rstx32@gmail.com>
2020-03-13 08:05:47 +0700

    update enkripsi menggunakan tabel

--- app/src/main/java/com/example/substitusi_kriptografi/dekripsi.java
+++ app/src/main/java/com/example/substitusi_kriptografi/dekripsi.java
@@ -20,23 +20,44 @@ public class dekripsi extends AppCompatActivity {
     }
 
     public void dekripsi_button(View view) {
-        int x,y;
-        String cipher = ciphertext.getText().toString();
-        char[] tampung = new char[ciphertext.length()];
+//        int x,y;
+//        String cipher = ciphertext.getText().toString();
+//        char[] tampung = new char[ciphertext.length()];
+//
+//        for(int i=0; i<ciphertext.length(); i++){
+//            x = cipher.charAt(i)-5;
+//            if(x<97)
+//                x+=26;
+//
+//            y = x-3;
+//            if(y<97)
+//                y+=26;
+//
+//            if(cipher.charAt(i)==' '){
+//                tampung[i] = ' ';
+//            }else{
+//                tampung[i] = (char)y;
+//            }
+//        }
+//        String hasil = new String(tampung);
+//        plaintext.setText(hasil+"");
 
-        for(int i=0; i<ciphertext.length(); i++){
-            x = cipher.charAt(i)-5;
-            if(x<97)
-                x+=26;
+//      ***MENGGUNAKAN TABEL***
+        String cipher    = ciphertext.getText().toString();
+        char[] tampung  = new char[cipher.length()];
 
-            y = x-3;
-            if(y<97)
-                y+=26;
-
-            if(cipher.charAt(i)==' '){
-                tampung[i] = ' ';
-            }else{
-                tampung[i] = (char)y;
+        String alfabet  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String key1     = "defghijklmnopqrstuvwxyzabcDEFGHIJKLMNOPQRSTUVWXYZABC";
+        String key2     = "ijklmnopqrstuvwxyzabcdefghIJKLMNOPQRSTUVWXYZABCDEFGH";
+        for(int i=0; i<cipher.length(); i++) {
+            for (int j = 0; j < alfabet.length(); j++) {
+                if (cipher.charAt(i) == key2.charAt(j)) {
+                    tampung[i] = key1.charAt(j);
+                    tampung[i] = alfabet.charAt(j);
+                    break;
+                } else if (cipher.charAt(i) == ' ') {
+                    tampung[i] = ' ';
+                }
             }
         }
         String hasil = new String(tampung);
--- app/src/main/java/com/example/substitusi_kriptografi/enkripsi.java
+++ app/src/main/java/com/example/substitusi_kriptografi/enkripsi.java
@@ -19,24 +19,45 @@ public class enkripsi extends AppCompatActivity {
     }
 
     public void enkripsi_button(View view) {
-        int x, y;
+//        ***MENGGUNAKAN ASCII***
+//        int x, y;
+//        String plain    = plaintext.getText().toString();
+//        char[] tampung  = new char[plain.length()];
+//
+//        for(int i=0; i<plain.length(); i++){
+//            x = plain.charAt(i)+3;
+//            if(x>122)
+//                x-=26;
+//
+//            y = (x-92)%26+97;
+//            if(y>122)
+//                y-=26;
+//
+//            if(plain.charAt(i)==' '){
+//                tampung[i] = ' ';
+//            }else{
+//                tampung[i] = (char)y;
+//            }
+//        }
+//        String hasil = new String(tampung);
+//        ciphertext.setText(hasil+"");
+
+//        ***MENGGUNAKAN TABEL***
         String plain    = plaintext.getText().toString();
         char[] tampung  = new char[plain.length()];
 
-        // menggunakan ascii
-        for(int i=0; i<plain.length(); i++){
-            x = plain.charAt(i)+3;
-            if(x>122)
-                x-=26;
-
-            y = (x-92)%26+97;
-            if(y>122)
-                y-=26;
-
-            if(plain.charAt(i)==' '){
-                tampung[i] = ' ';
-            }else{
-                tampung[i] = (char)y;
+        String alfabet  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String key1     = "defghijklmnopqrstuvwxyzabcDEFGHIJKLMNOPQRSTUVWXYZABC";
+        String key2     = "ijklmnopqrstuvwxyzabcdefghIJKLMNOPQRSTUVWXYZABCDEFGH";
+        for(int i=0; i<plain.length(); i++) {
+            for (int j = 0; j < alfabet.length(); j++) {
+                if (plain.charAt(i) == alfabet.charAt(j)) {
+                    tampung[i] = key1.charAt(j);
+                    tampung[i] = key2.charAt(j);
+                    break;
+                } else if (plain.charAt(i) == ' ') {
+                    tampung[i] = ' ';
+                }
             }
         }
         String hasil = new String(tampung);
